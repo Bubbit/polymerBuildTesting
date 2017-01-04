59a016fec386b63027169925f38b2b9249d17cfd
Remco <Remco@guanabanas-mbp.dhcp.ing-ned>
2017-01-04 10:06:18 +0100

    Work around 2

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const del = require('del');
 const gulp = require('gulp');
 const logging = require('plylog');
 const gulpif = require('gulp-if');
+const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const cssSlam = require('css-slam').gulp;
 const htmlMinifier = require('gulp-html-minifier');
@@ -25,7 +26,8 @@ function html() {
     removeEmptyAttributes: true,
     removeScriptTypeAttributes: true,
     removeStyleLinkTypeAttributes: true,
-    removeOptionalTags: true
+    removeOptionalTags: true,
+    minifyCSS: true
   });
 }
 
@@ -39,18 +41,23 @@ function waitFor(stream) {
   });
 }
 
-let project = new PolymerProject({
-  root: process.cwd(),
-  entrypoint: 'app/index.html',
-  shell: 'app/src/app-main/app-main.html',
-});
-
 // Clean build directory
 gulp.task('clean', () => {
   return del('build');
 });
 
-gulp.task('test1', (cb) => {
+gulp.task('unbundled', (cb) => {
+
+  let project = new PolymerProject({
+    root: process.cwd(),
+    entrypoint: 'index.html',
+    shell: 'src/app-main/app-main.html',
+    extraDependencies: ['bower_components/webcomponentsjs/webcomponents-lite.js'],
+    "sources": [
+      "src/**/*",
+      "bower.json"
+    ]
+  });
 
   let swConfig = {
     staticFileGlobs: [
@@ -61,20 +68,29 @@ gulp.task('test1', (cb) => {
     navigateFallback: '/index.html',
   };
 
-  // merge the source and dependencies streams to we can analyze the project
-  let allFiles = mergeStream(project.sources(), project.dependencies());
+  // process source files in the project
+  let sources = project.sources()
+    .pipe(project.splitHtml())
+    .pipe(gulpif(['**/*.js', '!**/*.min.js'], babel({ presets: ['es2015-nostrict'] })))
+    .pipe(gulpif(['**/*.js', '!**/*.min.js'], uglify({
+      preserveComments: function (node, comment) {
+        return /@polymerBehavior/.test(comment.value);
+      }
+    })))
+    .pipe(project.rejoinHtml());
 
-  // fork the stream in case downstream transformers mutate the files
-  // this fork will vulcanize the project
-  let bundledPhase =
-    fork(allFiles)
-      .pipe(project.bundler)
-      .pipe(project.splitHtml())
-      .pipe(gulpif(/\.js$/, uglify()))
-      .pipe(gulpif(/\.css$/, cssSlam()))
-      .pipe(gulpif('*.html', html()))
-      .pipe(project.rejoinHtml())
-      .pipe(gulp.dest('build/bundled'));
+  // process dependencies
+  let dependencies = project.dependencies()
+    .pipe(project.splitHtml())
+    .pipe(gulpif(['**/*.js', '!**/*.min.js'], babel({ presets: ['es2015-nostrict'] })))
+    // .pipe(gulpif(['**/*.js', '!**/*.min.js'], uglify({preserveComments: function (node, comment) {
+    //       return /@polymerBehavior/.test(comment.value);
+    // }})))
+        .pipe(gulpif(['**/*.js', '!**/*.min.js'], uglify({preserveComments: 'all'})))
+    .pipe(project.rejoinHtml());
+
+  // merge the source and dependencies streams to we can analyze the project
+  let allFiles = mergeStream(sources, dependencies);
 
   let unbundledPhase =
     fork(allFiles)
@@ -93,16 +109,61 @@ gulp.task('test1', (cb) => {
     });
   });
 
-  // Once the bundled build stream is complete, create a service worker for the
-  // build
-  let bundledPostProcessing = waitFor(bundledPhase).then(() => {
-    return addServiceWorker({
-      project: project,
-      buildRoot: 'build/bundled',
-      swConfig: swConfig,
-      bundled: true,
-    });
+  return unbundledPostProcessing;
+});
+
+gulp.task('bundled', (cb) => {
+
+  let project = new PolymerProject({
+    root: process.cwd() + '/build/unbundled/',
+    entrypoint: 'index.html',
+    shell: 'src/app-main/app-main.html',
+    extraDependencies: ['bower_components/webcomponentsjs/webcomponents-lite.js'],
+    "sources": [
+      "src/**/*",
+      "bower.json"
+    ]
   });
 
-  return Promise.all([unbundledPostProcessing, bundledPostProcessing]);
+  // let swConfig = {
+  //   staticFileGlobs: [
+  //     '/index.html',
+  //     '/shell.html',
+  //     '/source-dir/**',
+  //   ],
+  //   navigateFallback: '/index.html',
+  // };
+
+  // merge the source and dependencies streams to we can analyze the project
+  let allFiles = mergeStream(project.sources(), project.dependencies());
+
+  // fork the stream in case downstream transformers mutate the files
+  // this fork will vulcanize the project
+  let bundledPhase =
+    fork(allFiles)
+      .pipe(project.bundler)
+      .pipe(project.splitHtml())
+      // .pipe(gulpif(/\.css$/, cssSlam()))
+      .pipe(gulpif('*.html', html()))
+      .pipe(project.rejoinHtml())
+      .pipe(gulp.dest('build/bundled'));
+
+  // Once the bundled build stream is complete, create a service worker for the
+  // // build
+  // let bundledPostProcessing = waitFor(bundledPhase).then(() => {
+  //   return addServiceWorker({
+  //     project: project,
+  //     buildRoot: 'build/bundled',
+  //     swConfig: swConfig,
+  //     bundled: true,
+  //   });
+  // });
+
+  return bundledPhase;
 });
+
+gulp.task('default', gulp.series([
+    'clean',
+    'unbundled',
+    'bundled'
+]));
